14a765a19f0249963e4f51d15898071bb46a1597
s2moon98 <s2moon98@gmail.com>
2025-01-14 15:14:26 +0900

    Initial Commit

---
+++ timer/timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  timerApp.swift
+//  timer
+//
+//  Created by pond.je on 1/14/25.
+//
+
+import SwiftUI
+
+@main
+struct timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ timerTests/timerTests.swift
@@ -0,0 +1,17 @@
+//
+//  timerTests.swift
+//  timerTests
+//
+//  Created by pond.je on 1/14/25.
+//
+
+import Testing
+@testable import timer
+
+struct timerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
